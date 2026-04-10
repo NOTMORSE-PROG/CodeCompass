743a01ccfb785975929bf9862502b9bc3f545d8c
Mark <markandreicondino@gmail.com>
2026-04-10 16:27:49 +0800

    fix(deploy): clean up build/start commands for Python runtime

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-VENV=/opt/render/project/src/.venv
-
-# Install into Render's venv explicitly
-$VENV/bin/pip install -r requirements.txt
-$VENV/bin/python manage.py collectstatic --noinput
-$VENV/bin/python manage.py migrate
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
